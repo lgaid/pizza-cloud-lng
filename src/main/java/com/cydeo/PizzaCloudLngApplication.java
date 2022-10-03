af50bf7ca15535c1cf87625a8697a0fe95c4f3d0
lgaid <104643045+lgaid@users.noreply.github.com>
2022-10-03 12:05:35 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/PizzaCloudLngApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaCloudLngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaCloudLngApplication.class, args);
+    }
+
+}
